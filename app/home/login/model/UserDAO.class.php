66dc90162a0b9e9a975fb48ffd7a1d4e5971d01d
Tiago Silva <44373482+tigosante@users.noreply.github.com>
2020-05-10 02:00:09 -0300

    ajuste.

--- app/home/login/model/UserDAO.class.php
+++ app/home/login/model/UserDAO.class.php
@@ -25,3 +25,51 @@ class UserDAO extends AbModel
         return ($comando->execute($this->mUser->get_parametos()) && $comando->rowCount() === 1);
     }
 }
+
+
+
+/*
+<?php
+require_once(ROOT . "/app/core/classes/abstracts/AbModel.php");
+require_once(ROOT . "/app/home/login/model/userO.class.php");
+
+class UserDAO extends AbModel
+{
+    private $oUser;
+
+    public function __construct()
+    {
+        $this->oUser = new UserO;
+        parent::__construct();
+    }
+
+    public function verificar_user(): bool
+    {
+        $this->oUser->set_all_parametros();
+
+        $id         = $this->oUser->get_id();
+        $ds_email   = $this->oUser->get_ds_email();
+        $senha_user = $this->oUser->get_senha_user();
+
+        $sql = "SELECT * FROM `USUARIO` WHERE 1=1 ";
+
+        $sql .= $id > -1    ? " AND ID         = :ID "         : "";
+        $sql .= $ds_email   ? " AND DS_EMAIL   = :DS_EMAIL "   : "";
+        $sql .= $senha_user ? " AND SENHA_USER = :SENHA_USER " : "";
+
+        $comando = $this->pdo->prepare($sql);
+
+        $parametros = [
+            ":DS_EMAIL" => $ds_email,
+            ":SENHA_USER" => $senha_user
+        ];
+
+        if ($id > -1) {
+            $parametros[":ID"] = $id;
+        }
+
+        return ($comando->execute($parametros) && $comando->rowCount() === 1);
+    }
+}
+
+*/
