3ee1111b218ac43fb0e378c08c611960ee8f2a51
Tiago Silva <tsilvasantos38@gmail.com>
2020-06-17 11:41:22 -0300

    ajustes nos métodos.

--- app/config/conexoes/ConexaoOracle.class.php
+++ app/config/conexoes/ConexaoOracle.class.php
@@ -17,50 +17,46 @@ use core\classes\interfaces\IConexao;
  * Objeto que realiza a conexão com o Oracle DB e implementa a interface IConexao.
  * Cria a conexão usando PDO - PHP Data Objects.
  */
-class ConexaoOracle implements IConexao
+abstract class ConexaoOracle implements IConexao
 {
+    /**
+     * @param PDO $conexao
+     */
     private static $conexao;
-
-    // private static $dsn = "mysql:host=localhost;dbname=mysql";
-    // private static $options = [\PDO::FETCH_ASSOC];
-    // private static $username = "tiago";
-    // private static $password = "0TwU9XYElaK8AIms";
 
     private static $dsn = "mysql:host=localhost;dbname=mysql";
     private static $options = [\PDO::FETCH_ASSOC];
     private static $username = "USR_TREINAMNETO";
     private static $password = "fhX7VgCniXfkQ8nb";
 
-    public function __construct()
-    {
-        try {
-            self::$conexao = new \PDO(self::$dsn, self::$username, self::$password, self::$options);
-        } catch (\PDOException $e) {
-            $this->errorConexao($e);
-        }
-    }
-
     /**
      * Método que verifica se existe a instância de uma determinada conexão.
      * Retorna a mesma caso já exista.
      * Retorna uma nova caso não exista.
      */
-    public static function getInstance()
+    public static function getInstance(): \PDO
     {
         if (!isset(self::$conexao)) {
-            self::$conexao = new \PDO(self::$dsn, self::$username, self::$password);
+            self::criarConexao();
         }
 
         return self::$conexao;
     }
 
+    private static function criarConexao(): void
+    {
+        try {
+            self::$conexao = new \PDO(self::$dsn, self::$username, self::$password, self::$options);
+        } catch (\PDOException $e) {
+            self::errorConexao($e);
+        }
+    }
+
     /**
      * Responsável por informar os erros de conexão.
      */
-    public function errorConexao($error)
+    public static function errorConexao($error): void
     {
-        $error_message = $error->getMessage();
-        echo $error_message;
-        exit;
+        throw new \InvalidArgumentException("Error conexão:\n" . $error->getMessage(), 1);
     }
 }
