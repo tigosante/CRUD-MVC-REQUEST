238b3c4d75e37141b40201019c6c6a4b157feb80
Tiago Silva <44373482+tigosante@users.noreply.github.com>
2020-09-02 18:00:37 -0300

    Limpeza do projeto.

--- core/Interfaces/TableObject/TableObjectInterface.php
+++ core/Interfaces/TableObject/TableObjectInterface.php
@@ -4,7 +4,11 @@ namespace core\interfaces\TableObject;
 
 interface TableObjectInterface
 {
-  public function loadData(): bool;
-  public function cleanData(): bool;
-  public function loadDataFromArray(array $dataArray): bool;
+  public function cleanData(string $tableColumnName): bool;
+  public function cleanAllData(): bool;
+
+  public function setData(string $tableColumnName): bool;
+  public function setAllData(): bool;
+
+  public function setAllDataFromArray(array $dataArray): bool;
 }
