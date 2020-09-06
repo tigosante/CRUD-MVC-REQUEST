9e052a22612950c6cab36b5be98240e117c1b235
tigosante <tsilvasantos38@gmail.com>
2020-09-06 15:56:14 -0300

    .

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "/app/config/autoloads/autoload_default.php";
-
-$conexao = config\conexoes\ConexaoOracle::getInstance();
-
-if (isset($conexao)) {
-    require_once($_SERVER["DOCUMENT_ROOT"] . "/lib/home/page/home.php");
-}
+var_dump("Em devesenvolvimento!");
