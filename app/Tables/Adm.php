a4f13485150ef96cde15505c25342a334cdc6b00
Tiago Silva <44373482+tigosante@users.noreply.github.com>
2020-09-08 18:54:16 -0300

    Criação de implementação exemplo

---
+++ app/Tables/Adm.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\Tables;
+
+use core\TableObject\Table;
+
+class Adm extends Table
+{
+  public const ID = "ID";
+  public const ADM_NAME = "ADM_NAME";
+  public const DT_CRIACAO = "DT_CRIACAO";
+
+  /**
+   * @var int $id
+   */
+  private $id;
+
+  /**
+   * @var string $adm_name
+   */
+  private $adm_name;
+
+  /**
+   * @var string $dt_cricao
+   */
+  private $dt_cricao;
+
+  public function __construct()
+  {
+    $tableName = "ADM";
+    $tableColumns = array(self::ID, self::DT_CRIACAO, self::ADM_NAME);
+
+    $tableConfiguration = array(
+      "tableName" => $tableName,
+      "tableColumns" => $tableColumns,
+      "tableIdentifier" => self::ID,
+      "dataBaseName" => ".CPG"
+    );
+
+    parent::__construct($tableConfiguration, $this);
+  }
+
+  public function get_id(): ?int
+  {
+    return $this->id;
+  }
+
+  public function set_id(int $id): void
+  {
+    $this->id = $id;
+  }
+
+  public function get_adm_name(): ?string
+  {
+    return $this->adm_name;
+  }
+
+  public function set_adm_name(string $adm_name): void
+  {
+    $this->adm_name = $adm_name;
+  }
+
+  public function get_dt_cricao(): ?string
+  {
+    return $this->dt_cricao;
+  }
+
+  public function set_dt_cricao(int $dt_cricao): void
+  {
+    $this->dt_cricao = $dt_cricao;
+  }
+}
--- index.php
+++ index.php
@@ -1,2 +1,60 @@
 <?php
-var_dump("Em desenvolvimento!");
+
+use app\Tables\Adm;
+
+$tableAdm = new Adm;
+
+$tableAdm->set_adm_name("Tiago");
+$tableAdm->set_dt_cricao((new DateTime())->format('Y-m-d'));
+
+// Ptambém podemos usar o método setData qe recebe um array de chave e valor.
+$tableAdm->setData(array(
+  $tableAdm::ADM_NAME => "Tiago",
+  $tableAdm::DT_CRIACAO => $date->format('Y-m-d')
+));
+
+// Criação de novo registro de usuário.
+$tableAdm->create();
+
+//  ---------------------------------------------------------------------------------------
+
+// Podemos usar por encapsulamento.
+$tableAdm->set_id(1);
+$tableAdm->set_adm_name("Pedro");
+$tableAdm->set_dt_cricao((new DateTime())->format('Y-m-d'));
+
+$tableAdm->where($tableAdm::ID . "= :ID")->update();
+
+//  ---------------------------------------------------------------------------------------
+
+$tableAdm->setData(array(
+  $tableAdm::ADM_NAME => "Tiago",
+  $tableAdm::DT_CRIACAO => $date->format('Y-m-d')
+));
+
+// Criação de novo registro de usuário.
+$tableAdm->create();
+
+//  ---------------------------------------------------------------------------------------
+
+$tableAdm->set_id(2);
+
+// Remoção de registro de usuário.
+$tableAdm->where($tableAdm::ID . "= :ID")->delete();
+
+//  ---------------------------------------------------------------------------------------
+// Busca de registro no banco de dados.
+
+$tableAdm->setData(array($tableAdm::ID => 1, $tableAdm::DT_CRIACAO => "07/09/2020"));
+
+//  Busca de vários registros com where.
+$tableAdm->where($tableAdm::DT_CRIACAO . " > :DT_CRIACAO")->findAll();
+
+//  Busca de todos os registros pegando apenas a coluna ADM_NAME.
+$tableAdm->findAll(array($tableAdm::ADM_NAME));
+
+//  Busca de um único registro no banco de dados.
+$tableAdm->set_id(1);
+$tableAdm->find($tableAdm->get_id());
+
+//  ---------------------------------------------------------------------------------------
